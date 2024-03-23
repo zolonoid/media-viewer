8197f9bd73987c46e32c46fd32e7eb5d051a9a6c
John Doe <jd@mail.web>
2024-03-23 19:16:17 +0300

    completed

--- src/components/MediaViewer.vue
+++ src/components/MediaViewer.vue
@@ -1,37 +1,60 @@
 <template>
-    <div>
+    <div class="media-viewer">
         
     </div>
 </template>
 
 <script>
 import ContentManager from './ContentManager.vue';
-import AudioPlayer from './AudioPlayer.vue';
-import VideoPlayer from './VideoPlayer.vue';
-import ImagePlayer from './ImagePlayer.vue';
+import PlayerAudio from './PlayerAudio.vue';
+import PlayerVideo from './PlayerVideo.vue';
+import PlayerImage from './PlayerImage.vue';
+
 export default {
-    
+    components:{
+        ContentManager,
+        PlayerAudio,
+        PlayerVideo,
+        PlayerImage
+    },
+    props:{
+        config:Object, //{content[]:{id,type,preview,title,src},filters[]:{id,icon,title,values[]:{id,icon,title,quick}}
+        position:Object  //{place,mainWidth,mainHeght,contentWidth,contentHeght}}
+    }
 }
 </script>
 
 <style lang="scss">
     .media-viewer{
-        &_cm-right{
-            &-visible{
+        position: relative;
+        &__player{
+            height: 100%;
+        }
+        &__nav-left,&__nav-right{
+            position: absolute;
+            height: 100%;
+        }
+        &__content{
+            position: absolute;
+            &_hidden{
+                display: none;
+            }
+            &_right{
+                width: 100%;
+                top: 0px;
+                left: 100%;
+            }
+            &_bottom{
+                top: 100%;
+                left: 0px;
+            }
+        }
 
-            }
-            &-hidden{
-                
-            }
-        }
-        &_cm-bottom{
+        // &_cm-inner{
             
-        }
-        &_cm-inner{
+        // }
+        // &_cm-fullscreen{
             
-        }
-        &_cm-fullscreen{
-            
-        }
+        // }
     }
 </style>
